72ff62820f9dc0a214a7ad0d1c85139612db65cb
Marwa Khalid <marwakhalid558@gmail.com>
2026-02-05 13:13:15 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import Signup from './modules/Signup/signup';
 import LoginPage from './modules/Login/login';
-import Dashboard from './modules/Dashboard/Dashboard';
+import Dashboard from './modules/Dashboard/dashboard';
 import MainLayout from './Layout/layout';
 // import Claims from './modules/Claims/ClaimsList';
 // import ProfileSetting from './modules/Setting/ProfileSetting/ProfileSetting';
